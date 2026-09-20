4fb99baea8ef831c7dca9b909670e965cb9ee61d
MariamAlix24 <alimariem59@gmail.com>
2026-09-20 20:01:38 +0300

    Question1(a&b)

--- G-ASP-NET-99-OOP-04/Program.cs
+++ G-ASP-NET-99-OOP-04/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-
+            #region Part 01 — Theoretical Questions
+            #region Question1
+            //a)Abstraction hides complex implementation details and exposes only the essential features of an object.It focuses on what an object does rather than how it works.
+            //b)1-Reduces Complexity: Keeps code simple by hiding unnecessary internal logic.
+            //2-Improves Maintainability: Allows underlying code changes without breaking external interfaces.
+            //3-Enhances Security: Restricts direct access to sensitive internal mechanisms.
+            #endregion
+            #endregion
         }
     }
 }
